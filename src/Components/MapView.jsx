64c760e3082cc7b0cf8d24b9f88004d8e67019de
Matthew Stobo <>
2019-01-28 00:37:43 +1100

    Added Modal and basic modal styles. Modal iterates address values, except lat / long. Empty MapView.jsx added.

---
+++ src/Components/MapView.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class MapView  extends Component {
+
+    render() {
+      return (
+        <section className="item-map">
+
+        </section>  
+      );
+    }
+  }
+
+  export default MapView
+
+
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+let checkShowHide = (showHide) => {
+    return showHide ? "item-modal modal-display" : "item-modal modal-hide"
+}
+
+const printAddress = (item) => {
+    let address = [];
+
+    for(let line in item.Address){
+
+        if(line !== 'Latitude' && line !== 'Longitude'){
+        address.push(<li>{item.Address[line]}</li>);
+      }
+    }
+
+    return address;
+}
+
+
+class Modal  extends Component {
+
+    constructor(props){
+        super(props);
+
+        let showHide = this.props.show
+
+        let address = printAddress(this.props.item);
+
+        this.state = {
+            showHide: checkShowHide(showHide),
+            address: address
+        }
+
+    }
+
+    hideModal = () => {
+        this.setState({
+            showHide: 'item-modal modal-hide'
+        })
+    }
+
+
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            showHide: checkShowHide(nextProps.show)
+        })
+    }
+
+    render() {
+      return (
+        <section className={this.state.showHide}>
+            <div className="modal-container">
+            <h3>{this.props.item.Name}</h3>
+            <ul>
+               {this.state.address}
+            </ul>
+            <button onClick={this.hideModal} className="item-button">CLOSE</button>
+            </div>
+        </section>  
+      );
+    }
+  }
+
+  export default Modal
